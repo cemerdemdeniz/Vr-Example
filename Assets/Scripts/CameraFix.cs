ef10e8d586ad1e1c1bc984f6e1abc347f9e27635
Cem Erdem Deniz <c-e-d@outlook.com.tr>
2021-01-02 20:09:43 +0000

    Added Cam Position with moving car

---
+++ Assets/Scripts/CameraFix.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFix : MonoBehaviour
+{
+    public float distance = 5;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+     transform.position = transform.position + Camera.main.transform.forward * distance * Time.deltaTime;
+
+       
+
+
+
+        
+    }
+}
