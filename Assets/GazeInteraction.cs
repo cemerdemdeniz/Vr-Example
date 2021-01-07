6b5a2cf2f62babb059c1554e966428102eab6b24
Cem Erdem Deniz <c-e-d@outlook.com.tr>
2021-01-07 20:24:06 +0000

    Added new way and new car

--- Assets/GazeInteraction.cs
+++ Assets/GazeInteraction.cs
@@ -7,8 +7,7 @@ using UnityEngine.SceneManagement;
 public class GazeInteraction : MonoBehaviour
 {
     public float gazeTime = 2f;
-    private float timer;
-    private bool gazeAt;
+    
     void Start()
     {
         
@@ -17,37 +16,23 @@ public class GazeInteraction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gazeAt)
-        {
-            timer += Time.deltaTime;
-            if(timer >= gazeTime)
-            {
-                ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current),ExecuteEvents.pointerDownHandler);
-                timer = 0f;
-            }
-        }
+        
+       
     }
 
     public void PointerEnter()
     {
-        gazeAt = true;
-        Debug.Log("PointerEnter");
-    }
+        
 
-    public void PointerExit()
-    {
-        gazeAt = false;
-        Debug.Log("PointerExit");
-    }
-    public void PointerDown()
-    {
-        Debug.Log("PointerDown");
-    }
-
-    public void StartFunc()
-    {
         SceneManager.LoadScene(1);
+        Debug.Log("PointerEnter");
+        
+       
     }
+
+    
+
+   
     public void QuitApp()
     {
         Application.Quit();
